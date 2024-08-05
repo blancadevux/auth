5b997dcbb48d85f133ee9a1dd9c9477aef1657c0
Blanca Pinot <bpinot@indexar.cl>
2024-08-05 19:55:47 -0400

    primero commit

---
+++ src/components/RegisterComponent.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <h2 class="mt-4">Registro</h2>
+        <form>
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Correo</label>
+            <input
+              id="exampleInputEmail1"
+              type="email"
+              class="form-control"
+              v-model="email"
+            />
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label"
+              >Contraseña</label
+            >
+            <input
+              id="exampleInputPassword1"
+              type="password"
+              class="form-control"
+              v-model="password"
+            />
+          </div>
+          <button @click="register" class="btn btn-primary">Registrar</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { createUserWithEmailAndPassword, auth } from '../auth';
+
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
+  methods: {
+    async register() {
+      try {
+        const userCredential = await createUserWithEmailAndPassword(
+          auth,
+          this.email,
+          this.password
+        );
+        const user = userCredential.user;
+        console.log(user);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+form {
+  width: 28rem;
+}
+</style>
---
+++ src/firebaseconfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDrlfM1X8GO3vv78qlLwFH13tof9VWsVzA',
+  authDomain: 'auth-13b9f.firebaseapp.com',
+  projectId: 'auth-13b9f',
+  storageBucket: 'auth-13b9f.appspot.com',
+  messagingSenderId: '643185802820',
+  appId: '1:643185802820:web:4e726f9af60fa4e6330611',
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp;
